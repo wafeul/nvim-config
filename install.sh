62e10eed2378da8ccf02602dafabbde82b006abe
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-01-16 14:13:19 +0100

    Save old config before installing the new one

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+if [ -d ~/.config/nvim ]
+then
+    tar -czf ~/.config/nvim-old-config.tgz ~/.config/nvim
+    rm -rf ~/.config/nvim
+    echo "Old nvim config saved in ~/.config/nvim-old-config.tgz"
+    if [ -d ~/.local/share/nvim ]
+    then
+        rm -rf ~/.local/share/nvim
+	echo "Old local files deleted"
+    fi
+fi
+
 cp -r nvim ~/.config/
