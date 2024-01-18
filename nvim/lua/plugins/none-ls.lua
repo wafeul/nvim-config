7557b4be3e941fc44fb199d09bfc0ad68199c0a9
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-01-18 09:08:27 +0100

    comments

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,6 +6,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.easy_codings_tandard,
+				-- null_ls.builtins.diagnostics.easy_coding_standard,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
