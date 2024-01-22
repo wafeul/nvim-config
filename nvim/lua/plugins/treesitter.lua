ac5afd38b463f71c02c1ba5aad0e694fe75e3575
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-01-22 08:49:07 +0100

    changed options according to lvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,17 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
         local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = "true" },
-			indent = { enable = "true" },
-		})
-	end,
+        config.setup({
+            rainbow = {
+                enable = false,
+                extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+                max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+            },
+            auto_install = true,
+            indent = { enable = true, disable = { "yaml", "python" } },
+            highlight = { enable = "true" },
+        })
+    end,
 }
