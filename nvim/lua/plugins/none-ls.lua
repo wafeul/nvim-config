a635cd00670ba7346ec13160203b7d801f5fffec
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-01-22 08:48:23 +0100

    added mason-null-ls and some formaters / linters

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,43 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		lazy = false,
+		config = function()
+			local null_ls = require("null-ls")
+			-- vim.cmd("MasonInstall beautysh stylua")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.beautysh,
+					null_ls.builtins.diagnostics.php,
+					null_ls.builtins.formatting.phpcbf,
+					null_ls.builtins.diagnostics.eslint_d,
+					null_ls.builtins.formatting.prettier,
+				},
+			})
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.formatting.easy_codings_tandard,
-				-- null_ls.builtins.diagnostics.easy_coding_standard,
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"beautysh",
+					"stylua",
+					"phpcbf",
+                    "eslint_d",
+                    "prettier",
+					"php",
+				},
+				handlers = {},
+			})
+		end,
+	},
 }
