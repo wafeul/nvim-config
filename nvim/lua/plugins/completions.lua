a2464ddcfe7a9bfdd4661014eec4fe23b4ca8e99
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-02-07 14:46:51 +0100

    adding config for completion

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,47 +1,53 @@
 return {
-    {
-        "hrsh7th/cmp-nvim-lsp"
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets"
-        }
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            cmp.setup({
-                snippet = {
-                    -- REQUIRED - you must specify a snippet engine
-                    expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    -- { name = "nvim_lsp" },
-                    -- { name = "vsnip" }, -- For vsnip users.
-                    { name = "luasnip" }, -- For luasnip users.
-                }, {
-                    { name = "buffer" },
-                }),
-            })
-        end,
-    },
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				formatting = {
+					fields = { "menu", "abbr", "kind" },
+					format = function(entry, item)
+						local menu_icon = {
+							nvim_lsp = "λ",
+							luasnip = "⋗",
+							buffer = "Ω",
+							path = "🖫",
+						}
+
+						item.menu = menu_icon[entry.source.name]
+						return item
+					end,
+				},
+				sources = cmp.config.sources({
+					{ name = "path" },
+					{ name = "nvim_lsp", keyword_length = 1 },
+					{ name = "buffer", keyword_length = 2 },
+					{ name = "luasnip", keyword_length = 2 },
+				}),
+			})
+		end,
+	},
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,77 +1,94 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup({
-                ui = {
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗"
-                    }
-                },
-                ensure_installed = {
-                    "json-to-sruct",
-                    "easy-coding-standard",
-                },
-            })
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+				ensure_installed = {
+					"json-to-sruct",
+					"easy-coding-standard",
+				},
+			})
+		end,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "vimls",
-                    "phpactor",
-                    "jsonls",
-                    "angularls",
-                    "html",
-                    "bashls",
-                },
-            })
-        end,
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"vimls",
+					"phpactor",
+					"jsonls",
+					"angularls",
+					"html",
+					"bashls",
+				},
+			})
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			local sign = function(opts)
+				vim.fn.sign_define(opts.name, {
+					texthl = opts.name,
+					text = opts.text,
+					numhl = "",
+				})
+			end
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.angularls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities
-            })
-            lspconfig.bashls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.phpactor.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                init_options = {
-                    ["language_server_phpstan.enabled"] = false,
-                    ["language_server_psalm.enabled"] = false,
-                },
-            })
-            -- Key bindings
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-            vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-            vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-            vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.phpactor.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				init_options = {
+					["language_server_phpstan.enabled"] = false,
+					["language_server_psalm.enabled"] = false,
+				},
+			})
+
+			sign({ name = "DiagnosticSignError", text = "✘" })
+			sign({ name = "DiagnosticSignWarn", text = "▲" })
+			sign({ name = "DiagnosticSignHint", text = "⚑" })
+			sign({ name = "DiagnosticSignInfo", text = "»" })
+
+			vim.diagnostic.config({
+				virtual_text = true,
+				severity_sort = true,
+				float = {
+					border = "rounded",
+					source = "always",
+				},
+			})
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+		end,
+	},
 }
