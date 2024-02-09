d965f47d888fc194989a769a97db269c3abfb70b
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-02-09 14:39:31 +0100

    phpcsfixer config
    Added config file for phpcsfixer

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,94 +1,99 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-				ensure_installed = {
-					"json-to-sruct",
-					"easy-coding-standard",
-				},
-			})
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+                ensure_installed = {
+                    "json-to-sruct",
+                    "easy-coding-standard",
+                },
+            })
+        end,
+    },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"vimls",
-					"phpactor",
-					"jsonls",
-					"angularls",
-					"html",
-					"bashls",
-				},
-			})
-		end,
-	},
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "vimls",
+                    "phpactor",
+                    "jsonls",
+                    "angularls",
+                    "dockerls",
+                    "html",
+                    "bashls",
+                },
+            })
+        end,
+    },
 
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			local sign = function(opts)
-				vim.fn.sign_define(opts.name, {
-					texthl = opts.name,
-					text = opts.text,
-					numhl = "",
-				})
-			end
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            local sign = function(opts)
+                vim.fn.sign_define(opts.name, {
+                    texthl = opts.name,
+                    text = opts.text,
+                    numhl = "",
+                })
+            end
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.angularls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.phpactor.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-				init_options = {
-					["language_server_phpstan.enabled"] = false,
-					["language_server_psalm.enabled"] = false,
-				},
-			})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.angularls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.bashls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.dockerls.setup({
+                cmd = { "docker-langserver", "--stdio" },
+                filetypes = { "Dockerfile", "dockerfile" }
+            })
+            lspconfig.phpactor.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                init_options = {
+                    ["language_server_phpstan.enabled"] = false,
+                    ["language_server_psalm.enabled"] = false,
+                },
+            })
 
-			sign({ name = "DiagnosticSignError", text = "✘" })
-			sign({ name = "DiagnosticSignWarn", text = "▲" })
-			sign({ name = "DiagnosticSignHint", text = "⚑" })
-			sign({ name = "DiagnosticSignInfo", text = "»" })
+            sign({ name = "DiagnosticSignError", text = "✘" })
+            sign({ name = "DiagnosticSignWarn", text = "▲" })
+            sign({ name = "DiagnosticSignHint", text = "⚑" })
+            sign({ name = "DiagnosticSignInfo", text = "»" })
 
-			vim.diagnostic.config({
-				virtual_text = true,
-				severity_sort = true,
-				float = {
-					border = "rounded",
-					source = "always",
-				},
-			})
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+            vim.diagnostic.config({
+                virtual_text = true,
+                severity_sort = true,
+                float = {
+                    border = "rounded",
+                    source = "always",
+                },
+            })
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
-			vim.lsp.handlers["textDocument/signatureHelp"] =
-				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-		end,
-	},
+            vim.lsp.handlers["textDocument/signatureHelp"] =
+                vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+        end,
+    },
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,47 +1,46 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		lazy = false,
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.beautysh,
-					null_ls.builtins.diagnostics.php,
-                    null_ls.builtins.formatting.phpcsfixer,
-                    -- null_ls.builtins.formatting.phpcsfixer.with({
-                    --     extra_args = {"--config=/home/rbourgeas/.config/nvim/externals/phpcsfixer.php"},
-                    -- }),
-					null_ls.builtins.diagnostics.eslint_d,
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.diagnostics.hadolint,
-				},
-			})
-		end,
-	},
+    {
+        "nvimtools/none-ls.nvim",
+        lazy = false,
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.beautysh,
+                    null_ls.builtins.diagnostics.php,
+                    null_ls.builtins.formatting.phpcsfixer.with({
+                        extra_args = {"--config", "/home/rbourgeas/.config/nvim/externals/phpcsfixer.php"},
+                    }),
+                    null_ls.builtins.diagnostics.eslint_d,
+                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.diagnostics.hadolint,
+                },
+            })
+        end,
+    },
 
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = {
-					"beautysh",
-					"stylua",
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        config = function()
+            require("mason-null-ls").setup({
+                ensure_installed = {
+                    "beautysh",
+                    "stylua",
                     "hadolint",
-					"phpcbf",
-					"phpcsfixer",
+                    "phpcbf",
+                    "phpcsfixer",
                     "eslint_d",
                     "prettier",
-					"php",
-				},
-				handlers = {},
-			})
-		end,
-	},
+                    "php",
+                },
+                handlers = {},
+            })
+        end,
+    },
 }
