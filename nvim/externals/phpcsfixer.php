fcfd1190c4f08f65fdb9214556eb7d75f1dfa020
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-02-14 12:23:40 +0100

    phpcsfixer new rule
    Added same line for function opening braces

--- nvim/externals/phpcsfixer.php
+++ nvim/externals/phpcsfixer.php
@@ -8,6 +8,7 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
         'control_structure_continuation_position' => ['position' => 'next_line'],
+        'braces_position' => ['functions_opening_brace' => 'same_line'],
     ])
     ->setFinder($finder)
 ;
