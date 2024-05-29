11f0e8ea4ef11434abf5fa47ab0a2f8e04210668
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-05-29 14:36:03 +0200

    added class_attributes_separation

--- nvim/externals/phpcsfixer.php
+++ nvim/externals/phpcsfixer.php
@@ -9,6 +9,9 @@ return (new PhpCsFixer\Config())
         '@PSR12' => true,
         'control_structure_continuation_position' => ['position' => 'next_line'],
         'braces_position' => ['functions_opening_brace' => 'same_line'],
+        'class_attributes_separation' => 
+            ['const' => 'only_if_meta',
+            'method' => 'one'],
     ])
     ->setFinder($finder)
 ;
