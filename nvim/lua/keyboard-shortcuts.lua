4b7cd3e576bf843d11c98bc8909094e549d2517f
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-07-30 10:28:32 +0200

    Adding shortcut to hide ESLints

--- nvim/lua/keyboard-shortcuts.lua
+++ nvim/lua/keyboard-shortcuts.lua
@@ -43,6 +43,17 @@ vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
+-- Disable ESLint LSP server and hide virtual text in Neovim
+-- Add this to your init.lua or init.vim file
+local isLspDiagnosticsVisible = true
+vim.keymap.set("n", "<leader>lx", function()
+    isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+    vim.diagnostic.config({
+        virtual_text = isLspDiagnosticsVisible,
+        underline = isLspDiagnosticsVisible,
+    })
+end)
+
 ----------------------------------------------------------------------------------
 -- Completion
 ----------------------------------------------------------------------------------
