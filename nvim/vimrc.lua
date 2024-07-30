6f5de4b8644df597b3e5e56c0846dd00c582ec57
Raphaël Bourgeas <rbourgeas@embl.fr>
2024-07-30 10:24:53 +0200

    indent

--- nvim/vimrc.lua
+++ nvim/vimrc.lua
@@ -4,28 +4,23 @@
 vim.opt.termguicolors = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
--- Disable ESLint LSP server and hide virtual text in Neovim
--- Add this to your init.lua or init.vim file
-local isLspDiagnosticsVisible = true
-vim.keymap.set("n", "<leader>lx", function()
-    isLspDiagnosticsVisible = not isLspDiagnosticsVisible
-    vim.diagnostic.config({
-        virtual_text = isLspDiagnosticsVisible,
-        underline = isLspDiagnosticsVisible
-    }) end)
-
-
+local function open_nvim_tree()
+    -- open the tree.
+    require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+end
+-- Call the open_nvim_tree function after all plugins has been loaded ("VimEnter"). If not here, not working with alpha plugin.
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 require("keyboard-shortcuts")
